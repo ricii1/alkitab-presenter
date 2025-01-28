ba5a6a3361b95cdbdb2f61b45e38c4e1d5c809c7
Junathan Richie <jonathanrichie05@gmail.com>
2025-01-28 15:02:33 +0700

    feat: alkitab presenter v1

---
+++ src/pages/api/db.ts
@@ -0,0 +1,188 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database('./indo_tb.sqlite'); // Pastikan file database Anda ada di lokasi ini.
+interface Data {
+    book: string;
+    chapter: number;
+    verse: number;
+}
+
+interface ResponseData {
+    book: number,
+    chapter: number,
+    id: number,
+    text: string,
+    verse: number
+};
+const numberToBooks = {
+    1: "Kejadian",
+    2: "Keluaran",
+    3: "Imamat",
+    4: "Bilangan",
+    5: "Ulangan",
+    6: "Yosua",
+    7: "Hakim-Hakim",
+    8: "Rut",
+    9: "1 Samuel",
+    10: "2 Samuel",
+    11: "1 Raja-Raja",
+    12: "2 Raja-Raja",
+    13: "1 Tawarikh",
+    14: "2 Tawarikh",
+    15: "Ezra",
+    16: "Nehemia",
+    17: "Ester",
+    18: "Ayub",
+    19: "Mazmur",
+    20: "Amsal",
+    21: "Pengkhotbah",
+    22: "Kidung Agung",
+    23: "Yesaya",
+    24: "Yeremia",
+    25: "Ratapan",
+    26: "Yehezkiel",
+    27: "Daniel",
+    28: "Hosea",
+    29: "Yoel",
+    30: "Amos",
+    31: "Obaja",
+    32: "Yunus",
+    33: "Mikha",
+    34: "Nahum",
+    35: "Habakuk",
+    36: "Zefanya",
+    37: "Hagai",
+    38: "Zakharia",
+    39: "Maleakhi",
+    40: "Matius",
+    41: "Markus",
+    42: "Lukas",
+    43: "Yohanes",
+    44: "Kisah Para Rasul",
+    45: "Roma",
+    46: "1 Korintus",
+    47: "2 Korintus",
+    48: "Galatia",
+    49: "Efesus",
+    50: "Filipi",
+    51: "Kolose",
+    52: "1 Tesalonika",
+    53: "2 Tesalonika",
+    54: "1 Timotius",
+    55: "2 Timotius",
+    56: "Titus",
+    57: "Filemon",
+    58: "Ibrani",
+    59: "Yakobus",
+    60: "1 Petrus",
+    61: "2 Petrus",
+    62: "1 Yohanes",
+    63: "2 Yohanes",
+    64: "3 Yohanes",
+    65: "Yudas",
+    66: "Wahyu"
+};
+
+const booksToNumbers = {
+    "Kejadian": 1,
+    "Keluaran": 2,
+    "Imamat": 3,
+    "Bilangan": 4,
+    "Ulangan": 5,
+    "Yosua": 6,
+    "Hakim-Hakim": 7,
+    "Rut": 8,
+    "1 Samuel": 9,
+    "2 Samuel": 10,
+    "1 Raja-Raja": 11,
+    "2 Raja-Raja": 12,
+    "1 Tawarikh": 13,
+    "2 Tawarikh": 14,
+    "Ezra": 15,
+    "Nehemia": 16,
+    "Ester": 17,
+    "Ayub": 18,
+    "Mazmur": 19,
+    "Amsal": 20,
+    "Pengkhotbah": 21,
+    "Kidung Agung": 22,
+    "Yesaya": 23,
+    "Yeremia": 24,
+    "Ratapan": 25,
+    "Yehezkiel": 26,
+    "Daniel": 27,
+    "Hosea": 28,
+    "Yoel": 29,
+    "Amos": 30,
+    "Obaja": 31,
+    "Yunus": 32,
+    "Mikha": 33,
+    "Nahum": 34,
+    "Habakuk": 35,
+    "Zefanya": 36,
+    "Hagai": 37,
+    "Zakharia": 38,
+    "Maleakhi": 39,
+    "Matius": 40,
+    "Markus": 41,
+    "Lukas": 42,
+    "Yohanes": 43,
+    "Kisah Para Rasul": 44,
+    "Roma": 45,
+    "1 Korintus": 46,
+    "2 Korintus": 47,
+    "Galatia": 48,
+    "Efesus": 49,
+    "Filipi": 50,
+    "Kolose": 51,
+    "1 Tesalonika": 52,
+    "2 Tesalonika": 53,
+    "1 Timotius": 54,
+    "2 Timotius": 55,
+    "Titus": 56,
+    "Filemon": 57,
+    "Ibrani": 58,
+    "Yakobus": 59,
+    "1 Petrus": 60,
+    "2 Petrus": 61,
+    "1 Yohanes": 62,
+    "2 Yohanes": 63,
+    "3 Yohanes": 64,
+    "Yudas": 65,
+    "Wahyu": 66
+};
+  
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'GET') {
+        res.status(405).json({ error: 'Method not allowed' });
+        return;
+    } else {
+        const { book, chapter, verseStart, verseEnd } = req.query;
+        if (!book || !chapter || !verseStart || !verseEnd) {
+            res.status(400).json({ error: 'Invalid request' });
+            return;
+        }
+        if (verseEnd && verseEnd < verseStart) {
+            res.status(400).json({ error: 'Invalid request' });
+            return;
+        }
+        const numberOfBook: number = booksToNumbers[book as keyof typeof booksToNumbers];
+        if(!numberOfBook) {
+            res.status(400).json({ error: 'Invalid request' });
+            return;
+        }
+        const query = 'SELECT * FROM verses WHERE book = ? AND chapter = ? AND verse >= ? AND verse <= ?';
+        db.all(query, [numberOfBook, chapter, verseStart, verseEnd], (err, rows: ResponseData[]) => {
+            if (err) {
+                console.error('Database query error:', err);
+                res.status(500).json({ error: 'Database query failed' });
+            } else {
+                const responseText = rows.map(row => row.text).join('\n');
+                console.log(responseText);
+                res.status(200).json(responseText);
+            }
+        });
+    }
+  
+  }
---
+++ src/pages/index.tsx
@@ -0,0 +1,258 @@
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [book, setBook] = useState("");
+  const [chapter, setChapter] = useState(1);
+  const [verseStart, setVerseStart] = useState(1);
+  const [verseEnd, setVerseEnd] = useState(1);
+  const [data, setData] = useState("");
+  const [error, setError] = useState(null);
+  const [fontSize, setFontSize] = useState(16);
+  const [textAlign, setTextAlign] = useState<"left" | "center" | "right">(
+    "left"
+  );
+  const [isBold, setIsBold] = useState(false);
+  const [spacing, setSpacing] = useState(0);
+  const [lineHeight, setLineHeight] = useState(1.5);
+
+  const books = [
+    "Kejadian",
+    "Keluaran",
+    "Imamat",
+    "Bilangan",
+    "Ulangan",
+    "Yosua",
+    "Hakim-Hakim",
+    "Rut",
+    "1 Samuel",
+    "2 Samuel",
+    "1 Raja-Raja",
+    "2 Raja-Raja",
+    "1 Tawarikh",
+    "2 Tawarikh",
+    "Ezra",
+    "Nehemia",
+    "Ester",
+    "Ayub",
+    "Mazmur",
+    "Amsal",
+    "Pengkhotbah",
+    "Kidung Agung",
+    "Yesaya",
+    "Yeremia",
+    "Ratapan",
+    "Yehezkiel",
+    "Daniel",
+    "Hosea",
+    "Yoel",
+    "Amos",
+    "Obaja",
+    "Yunus",
+    "Mikha",
+    "Nahum",
+    "Habakuk",
+    "Zefanya",
+    "Hagai",
+    "Zakharia",
+    "Maleakhi",
+    "Matius",
+    "Markus",
+    "Lukas",
+    "Yohanes",
+    "Kisah Para Rasul",
+    "Roma",
+    "1 Korintus",
+    "2 Korintus",
+    "Galatia",
+    "Efesus",
+    "Filipi",
+    "Kolose",
+    "1 Tesalonika",
+    "2 Tesalonika",
+    "1 Timotius",
+    "2 Timotius",
+    "Titus",
+    "Filemon",
+    "Ibrani",
+    "Yakobus",
+    "1 Petrus",
+    "2 Petrus",
+    "1 Yohanes",
+    "2 Yohanes",
+    "3 Yohanes",
+    "Yudas",
+    "Wahyu",
+  ];
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      console.log("Submitting", book, chapter, verseStart);
+      if (!book) throw new Error("Book must be selected");
+      if (!verseEnd) setVerseEnd(verseStart);
+      const query = new URLSearchParams({
+        book,
+        chapter: chapter.toString(),
+        verseStart: verseStart.toString(),
+        verseEnd: verseEnd.toString(),
+      }).toString();
+      const res = await fetch(`/api/db?${query}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!res.ok) {
+        console.log(res);
+        throw new Error("Failed to send request");
+      }
+      const data = await res.json();
+      console.log(data);
+      setError(null);
+      setData(data);
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold">Alkitab Presenter</h1>
+      <form onSubmit={handleSubmit} className="p-4">
+        <div className="flex justify-between w-1/2 mx-auto space-x-2">
+          <div className="border border-black p-2">
+            <label htmlFor="book">Book: </label>
+            <select
+              className="border border-black px-2"
+              id="book"
+              value={book}
+              onChange={(event) => setBook(event.target.value)}
+            >
+              <option value="">-- Pilih Kitab --</option>
+              {books.map((book) => (
+                <option key={book} value={book}>
+                  {book}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="border border-black p-2">
+            <label htmlFor="chapter">Chapter: </label>
+            <input
+              type="number"
+              min={1}
+              id="chapter"
+              value={chapter}
+              className="border border-black px-2"
+              onChange={(event) => setChapter(parseInt(event.target.value))}
+            />
+          </div>
+          <div className="border border-black p-2">
+            <label htmlFor="verseStart">Verse Start: </label>
+            <input
+              type="number"
+              id="verseStart"
+              min={1}
+              value={verseStart}
+              className="border border-black px-2"
+              onChange={(event) => setVerseStart(parseInt(event.target.value))}
+            />
+          </div>
+          <div className="border border-black p-2">
+            <label htmlFor="verseEnd">Verse End: </label>
+            <input
+              type="number"
+              id="verseEnd"
+              min={1}
+              value={verseEnd}
+              className="border border-black px-2"
+              onChange={(event) => setVerseEnd(parseInt(event.target.value))}
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-green-500 py-2 px-4 rounded-3xl my-2 font-bold w-1/4  hover:bg-green-700 hover:text-white"
+          >
+            Search
+          </button>
+        </div>
+        <div className="flex justify-center mt-2 space-x-2">
+          <div className="border border-black p-2">
+            <label htmlFor="fontSize">Font Size: </label>
+            <input
+              type="number"
+              min={1}
+              id="fontSize"
+              value={fontSize}
+              className="border border-black px-2"
+              onChange={(event) => setFontSize(parseInt(event.target.value))}
+            />
+          </div>
+          <div className="border border-black p-2">
+            <label htmlFor="textAlign">Text Align: </label>
+            <select
+              id="textAlign"
+              value={textAlign}
+              className="border border-black px-2"
+              onChange={(event) =>
+                setTextAlign(event.target.value as "left" | "center" | "right")
+              }
+            >
+              <option value="left">Left</option>
+              <option value="center">Center</option>
+              <option value="right">Right</option>
+            </select>
+          </div>
+          <div className="border border-black p-2">
+            <label htmlFor="bold">Bold: </label>
+            <input
+              type="checkbox"
+              id="bold"
+              checked={isBold}
+              className="ml-2"
+              onChange={() => setIsBold(!isBold)}
+            />
+          </div>
+          <div className="border border-black p-2">
+            <label htmlFor="spacing">Letter Spacing: </label>
+            <input
+              type="number"
+              min={0}
+              id="spacing"
+              value={spacing}
+              className="border border-black px-2"
+              onChange={(event) => setSpacing(parseInt(event.target.value))}
+            />
+          </div>
+          <div className="border border-black p-2">
+            <label htmlFor="lineHeight">Line Height: </label>
+            <input
+              type="number"
+              min={1}
+              id="lineHeight"
+              value={lineHeight}
+              className="border border-black px-2"
+              step="0.25"
+              onChange={(event) =>
+                setLineHeight(parseFloat(event.target.value))
+              }
+            />
+          </div>
+        </div>
+      </form>
+      <div
+        className="mt-4 px-28"
+        style={{
+          fontSize: `${fontSize}px`,
+          textAlign: textAlign,
+          fontWeight: isBold ? "bold" : "normal",
+          letterSpacing: `${spacing}px`,
+          lineHeight: lineHeight,
+          whiteSpace: "pre-wrap",
+        }}
+      >
+        {error && <p className="text-red font-bold">Error: {error}</p>}
+        {!error && data && <p>{data}</p>}
+      </div>
+    </div>
+  );
+}
